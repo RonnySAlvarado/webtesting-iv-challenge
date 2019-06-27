16e3aa20d182017cf47637ffa71a15a745d8e031
unknown <Rsalvarado777@gmail.com>
2019-06-27 17:17:14 -0500

    Created a usersModel.js file with all of the helper functions needed

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: "Up and running" })
 })
 
+server.get('/users', (req, res) => {
+    Users.getAll()
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
 module.exports = server;
